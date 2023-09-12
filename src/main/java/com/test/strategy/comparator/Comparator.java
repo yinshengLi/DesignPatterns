9dd5c37cd2c0297a218078a5e76c1f27242930b5
yinshengLi <1195979042@qq.com>
2023-09-12 09:34:53 +0800

    修改

--- src/main/java/com/test/strategy/comparator/Comparator.java
+++ src/main/java/com/test/strategy/comparator/Comparator.java
@@ -2,4 +2,12 @@ package com.test.strategy.comparator;
 
 public interface Comparator<T> {
     int compare(T o1,T o2);
+    //默认方法  可以解决接口升级的问题
+    default void m(){
+        System.out.println("1");
+    }
+    //静态方法  一般是工具方法
+    static void m2(){
+        System.out.println("2");
+    }
 }
--- src/main/java/com/test/strategy/comparator/TestMain.java
+++ src/main/java/com/test/strategy/comparator/TestMain.java
@@ -8,7 +8,18 @@ public class TestMain {
 
         Sorter2<Dog> sorter2 = new Sorter2<>();
         Dog[] dogArr = {new Dog(11),new Dog(5),new Dog(2)};
+        //从小到大
         sorter2.sort(dogArr,new DogCompator());
+        System.out.println(Arrays.toString(dogArr));
+        //使用lamba表达式,从大到小
+        sorter2.sort(dogArr,(o1,o2)->{
+            if(o1.age > o2.age){
+                return -1;
+            }else if(o1.age < o2.age){
+                return 1;
+            }
+            return 0;
+        });
         System.out.println(Arrays.toString(dogArr));
     }
 }
